e6199d6cdb515e255a9270c19d157aedb60a7d48
Joe Weber <wosephjeber@gmail.com>
2015-11-25 09:25:53 -0600

    Starting to add some inline documentation

--- js/lightTouch.js
+++ js/lightTouch.js
@@ -4,13 +4,20 @@ var LightTouch = function(elem, callback) {
   
   var jQuery = window.jQuery || null;
   
+  
+  /**
+   * Use a class to calculate and keep track of data about individual touches.
+  **/
+   
   var Touch = function() {
     var _this = this;
     
+    // variables needed for velocity calculation
     var prevDeltaX = 0;
     var prevDeltaY = 0;
     var prevTimestamp = 0;
     
+    // public properties
     this.type = null;
     this.startX = null;
     this.startY = null;
@@ -21,6 +28,7 @@ var LightTouch = function(elem, callback) {
     this.velocityX = 0;
     this.velocityY = 0;
     
+    // calculates the velocity of the touch event
     this.calculateVelocity = function(timestamp) {
       var deltaTime = timestamp - prevTimestamp;
       _this.velocityX = (_this.deltaX - prevDeltaX) / deltaTime;
@@ -31,6 +39,7 @@ var LightTouch = function(elem, callback) {
       prevDeltaY = _this.deltaY;
     };
   };
+  
   
   this.elem = jQuery && elem instanceof jQuery ? elem[0] : elem;
   this.callback = typeof callback === 'function' ? callback : noop;
@@ -84,6 +93,14 @@ var LightTouch = function(elem, callback) {
   bind(this.elem, 'touchstart mousedown', startHandler);
   bind(this.elem, 'touchend mouseup', endHandler);
   
+  
+  /** 
+   * Helper method to normalize adding event handlers with or without jQuery.
+   * If jQuery is loaded, it will use jQuery's bind() method. Otherwise it will
+   * use Javascripts native addEventListener method. If you need IE8 support,
+   * load jQuery and you'll be taken care of.
+  **/
+   
   function bind(element, evt, handler) {
     if (jQuery) {
       jQuery(element).bind(evt, handler);
@@ -95,6 +112,14 @@ var LightTouch = function(elem, callback) {
     }
   }
   
+  
+  /** 
+   * Helper method to normalize removing event handlers with or without jQuery.
+   * If jQuery is loaded, it will use jQuery's unbind() method. Otherwise it will
+   * use Javascripts native removeEventListener method. If you need IE8 support,
+   * load jQuery and you'll be taken care of.
+  **/
+   
   function unbind(element, evt, handler) {
     if (jQuery) {
       jQuery(element).unbind(evt, handler);
